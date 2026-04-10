b0758556fff4e34373143038cb58c0ff3228ae8c
IPlicho <3521528954@qq.com>
2026-04-10 14:06:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import telebot
-
-BOT_TOKEN = "8747559514:AAE_N9M9CallB4rYV0lbyny_0tGJnz3hLYU"
-bot = telebot.TeleBot(BOT_TOKEN)
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start'])  # 只处理 /start 指令
 def start(msg):
     bot.send_message(msg.chat.id, "✅ 机器人正常运行！")
-
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
