02d512152ff41e03e82e108f0d74b10e4a5e8ac8
IPlicho <3521528954@qq.com>
2026-04-10 14:03:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import telebot
 
-# 你的中间人机器人Token，和BotFather完全一致
 BOT_TOKEN = "8747559514:AAE_N9M9CallB4rYV0lbyny_0tGJnz3hLYU"
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -8,6 +7,5 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def start(msg):
     bot.send_message(msg.chat.id, "✅ 机器人正常运行！")
 
-# 启动，和你用户端完全一样的写法
 if __name__ == "__main__":
     bot.polling(none_stop=True)
