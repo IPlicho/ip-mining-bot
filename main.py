d5a0865e7d2b8072947e2d60f387a1959dca9075
IPlicho <3521528954@qq.com>
2026-04-10 14:25:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import telebot
 BOT_TOKEN = "8747559514:AAE_N9M9CallB4rYV0lbyny_0tGJnz3hLYU"
 bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(msg):
-    bot.send_message(msg.chat.id, "✅ 机器人正常运行！")
+@bot.message_handler(func=lambda msg: True)
+def all_msg(msg):
+    if msg.text == "/start":
+        bot.send_message(msg.chat.id, "✅ 机器人正常运行！")
 
 if __name__ == "__main__":
-    bot.polling(none_stop=True)
+    while True:
+        try:
+            bot.polling(none_stop=True, timeout=60)
+        except:
+            continue
